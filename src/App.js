b15930de8b4f03f54f398278e13a014d6941da32
rabbi31 <18103380@iubat.edu>
2021-11-18 15:27:46 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
+import React, { Fragment } from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes
+} from "react-router-dom";
 import Create from './Create';
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
+import BlogList from './BlogList';
+
 
 function App() {
   return (
+    
     <Router>
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
-            <Route exact path="/"> 
-              <Home />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/blogs/:id">
-              <BlogDetails />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-        </div>
+          <Routes>
+          
+            <Route path="/create" component={Create} />
+            <Route path="/blogs/:id" component={BlogDetails} />
+            <Route path="/bloglist"  component={BlogList} />
+            <Route path="*" component={NotFound} />
+            <Route exact path="/" component={Home} /> 
+          </Routes>
+         </div>
       </div>
     </Router>
+    
   );
 }
 
