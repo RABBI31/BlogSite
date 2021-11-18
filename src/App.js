80ed2f14360184e1b0349a5bf6c4e492f4c305ad
rabbi31 <18103380@iubat.edu>
2021-11-18 12:36:48 +0600

    Add nabar hompage and create page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <div className="content">
-          App Content
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/"> 
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
